af5d1cb99cf10d7e1b959df41002a830d0f7f3a3
Toni Bagur <toni.bagur@gmail.com>
2017-09-21 22:57:31 +0200

    .

--- train.py
+++ train.py
@@ -63,6 +63,58 @@ def train3(X_train,Y_train,offset_y_train,offset_x_train,X_dev,Y_dev,offset_y_de
                             offset_y_train[s], patches_dev, patches_train, regression_cost_x, regression_cost_y, sess,
                             slices_dev, slices_train, train_step)
 
+def train4(X_train,Y_train,X_dev,Y_dev,optimizer=tf.train.AdamOptimizer(0.0000001),batch_size=2000):
+    NUM_EPOCHS=10000
+    num_classes=Y_train.shape[1]
+
+    nx=X_train.shape[1]
+    y_logits,X,keep_prob=build_cnn_model2()
+
+    Y = tf.placeholder(tf.float32, [None, num_classes])
+    Ypred = tf.sigmoid(y_logits)
+    offset_y = tf.placeholder(tf.float32, [None, 1])
+    offset_x = tf.placeholder(tf.float32, [None, 1])
+    cost = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y_logits, labels=Y))
+    train_step = optimizer.minimize(cost)
+    correct_prediction = tf.equal(Y, tf.cast(tf.greater(Ypred, 0.5), "float"))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+    patches_dev, slices_dev = patches_of_image(DEV_FILE, 40, 40)
+    patches_train, slices_train = patches_of_image(TRAIN_FILE, 40, 40)
+
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        for i in range(NUM_EPOCHS):
+            num_full_batches=int(X_train.shape[0]/batch_size)
+            for j in range(num_full_batches):
+                s=slice(j*batch_size,(j+1)*batch_size)
+                train_batch2(X, X_dev[s], X_train[s], Y, Y_dev[s], Y_train[s], Ypred, accuracy, cost, i, keep_prob,
+                             patches_dev, patches_train, sess,slices_dev, slices_train, train_step)
+
+
+def train_batch2(X, X_dev, X_train, Y, Y_dev, Y_train, Ypred, accuracy, cost, i, keep_prob, patches_dev,
+                patches_train , sess, slices_dev, slices_train, train_step):
+    _, j, a= sess.run([train_step, cost, accuracy],
+                               feed_dict={X: X_train, Y: Y_train,
+                                          keep_prob: 0.5})
+    if i % 10 == 0:
+        print i, "train", j, a
+        print i, "test", sess.run([cost, accuracy], feed_dict={X: X_dev,Y: Y_dev, keep_prob: 1.})
+        Ydev = sess.run(Ypred,feed_dict={X: patches_dev.T, keep_prob: 1.})
+        Ytrain = sess.run(Ypred,feed_dict={X: patches_train.T, keep_prob: 1.})
+        im_dev = Image.open(DEV_FILE)
+        im_train = Image.open(TRAIN_FILE)
+        draw_dev = ImageDraw.Draw(im_dev)
+        draw_train = ImageDraw.Draw(im_train)
+        draw_rectangles_on_image((0,255,0), draw_dev, slices_dev[Ydev.T >= 0.5])
+        draw_rectangles_on_image((0, 255, 0), draw_train, slices_train[Ytrain.T >= 0.5])
+
+
+        draw_rectangles_on_image((255, 0, 0), draw_dev, slices_dev[Ydev.T >= 0.5])
+        draw_rectangles_on_image((255, 0, 0), draw_train, slices_train[Ytrain.T >= 0.5])
+
+        im_dev.save(DEV_FILE.replace('.png', "_segmented.png"))
+        im_train.save(TRAIN_FILE.replace('.png', "_segmented.png"))
 
 def train_batch(X, X_dev, X_train, Y, Y_dev, Y_train, Ypred, accuracy, cost, i, keep_prob, offset_x, offset_x_dev,
                 offset_x_pred, offset_x_train, offset_y, offset_y_dev, offset_y_pred, offset_y_train, patches_dev,
@@ -175,6 +227,34 @@ def build_cnn_model(image_shape=[40,40,3],channels_1=32,channels_2=64,conv_size_
     offset_x = linear_layer(h_fc1_drop, 1, num_hidden_fc)
 
     return y_conv,offset_y,offset_x,X,keep_prob
+
+
+def build_cnn_model2(image_shape=[40,40,3],channels_1=32,channels_2=64,conv_size_1=5,conv_size_2=5,num_hidden_fc=1024,num_classes=1):
+    final_conv_size = int(image_shape[0] / 2. / 2.)
+    nx = image_shape[0] * image_shape[1] * image_shape[2]
+    X = tf.placeholder(tf.float32, [None, nx], name="X")
+    x_image = tf.reshape(X, [-1, image_shape[0], image_shape[1], image_shape[2]])
+
+    W_conv1 = weight_variable([conv_size_1, conv_size_1, image_shape[2], channels_1])
+    b_conv1 = bias_variable([channels_1])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    W_conv2 = weight_variable([conv_size_2, conv_size_2, channels_1, channels_2])
+    b_conv2 = bias_variable([channels_2])
+
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)
+
+    W_fc1 = weight_variable([final_conv_size * final_conv_size * channels_2, num_hidden_fc])
+    b_fc1 = bias_variable([num_hidden_fc])
+    h_pool2_flat = tf.reshape(h_pool2, [-1, final_conv_size * final_conv_size * channels_2])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    keep_prob = tf.placeholder(tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    y_conv = linear_layer(h_fc1_drop, num_classes, num_hidden_fc)
+
+    return y_conv,X,keep_prob
 
 
 def linear_layer(input, num_outputs, num_input):
